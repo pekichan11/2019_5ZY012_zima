aa1b2df41e132135f294f598e3f97c8eca843647
Jan Janech <jan.janech@fri.uniza.sk>
2019-10-03 15:20:50 +0200

    02b - projekt BlueJ pre kalkulacku

---
+++ kalkulacka/kalkulacka/Kalkulacka.java
@@ -0,0 +1,15 @@
+
+/**
+ * Write a description of class Kalkulacka here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Kalkulacka {
+    /**
+     * Constructor for objects of class Kalkulacka
+     */
+    public Kalkulacka() {
+        // initialise instance variables
+    }
+}
