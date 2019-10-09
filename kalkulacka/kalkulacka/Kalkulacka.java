f6abde931cf1c647906568293d64a6c474386577
Jan Janech <jan.janech@fri.uniza.sk>
2019-10-09 11:06:05 +0200

    03a - praca s pamatou

--- kalkulacka/kalkulacka/Kalkulacka.java
+++ kalkulacka/kalkulacka/Kalkulacka.java
@@ -1,27 +1,45 @@
 public class Kalkulacka {
     private double medzicislo;
-    
+    private double pamat;
+
     public Kalkulacka(double prveCislo) {
         this.medzicislo = prveCislo;
+        this.pamat = 0;
     }
-    
+
     public void scitaj(double operand) {
         this.medzicislo += operand;
     }
-    
+
     public void odcitaj(double operand) {
         this.medzicislo -= operand;
     }
-    
+
     public void vynasob(double operand) {
         this.medzicislo *= operand;
     }
-    
+
     public void vydel(double operand) {
         this.medzicislo /= operand;
     }
-    
+
     public double vypluj() {
         return this.medzicislo;
+    }
+
+    public void mc() {
+        this.pamat = 0;
+    }
+
+    public void mr() {
+        this.medzicislo = this.pamat;
+    }
+
+    public void mPlus() {
+        this.pamat += this.medzicislo;
+    }
+
+    public void ms() {
+        this.pamat = this.medzicislo;
     }
 }
