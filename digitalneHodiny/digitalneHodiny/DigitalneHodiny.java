69d817cc29c18ad031c4e6a3be340a569e4ff461
Jan Janech <jan.janech@fri.uniza.sk>
2019-10-16 10:47:36 +0200

    04a - digitalky pocitaju so sekundami

--- digitalneHodiny/digitalneHodiny/DigitalneHodiny.java
+++ digitalneHodiny/digitalneHodiny/DigitalneHodiny.java
@@ -5,6 +5,7 @@
 public class DigitalneHodiny {
     private CiselnyDisplej hodiny;
     private CiselnyDisplej minuty;
+    private CiselnyDisplej sekundy;
     
     /**
      * Inicializuje digitalne hodiny. Cas zacina plynut od
@@ -13,16 +14,21 @@ public class DigitalneHodiny {
     public DigitalneHodiny() {
         this.hodiny = new CiselnyDisplej(24);
         this.minuty = new CiselnyDisplej(60);
+        this.sekundy = new CiselnyDisplej(60);
     }
     
     /**
      * Vykonava posunutie virtualneho casu o jednu minutu.
      */
     public void tik() {
-        this.minuty.krok();
+        this.sekundy.krok();
         
-        if (this.minuty.getHodnota() == 0) {
-            this.hodiny.krok();
+        if (this.sekundy.getHodnota() == 0) {
+            this.minuty.krok();
+            
+            if (this.minuty.getHodnota() == 0) {
+                this.hodiny.krok();
+            }
         }
     }
     
@@ -32,15 +38,16 @@ public class DigitalneHodiny {
      * @param hodina Hodina na ktoru sa ma cas nastavit.
      * @param minuta Minuta na ktoru sa ma cas nastavit.
      */
-    public void setCas(int hodina, int minuta) {
+    public void setCas(int hodina, int minuta, int sekunda) {
         this.hodiny.setHodnota(hodina);
         this.minuty.setHodnota(minuta);
+        this.sekundy.setHodnota(sekunda);
     }
     
     /**
      * Vrati cas vo forme retazca.
      */
     public String getCas() {
-        return this.hodiny.getHodnotaAkoRetazec() + ":" + this.minuty.getHodnotaAkoRetazec();
+        return this.hodiny.getHodnotaAkoRetazec() + ":" + this.minuty.getHodnotaAkoRetazec() + ":" + this.sekundy.getHodnotaAkoRetazec();
     }
 }
