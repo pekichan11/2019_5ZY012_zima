ff71f168520e1d2e6c86a3296790d54e27bdb27f
Jan Janech <jan.janech@fri.uniza.sk>
2019-10-17 15:36:26 +0200

    04b - implementacia 7segmentovej cislice

--- digitalneHodiny/digitalneHodiny/Cislica.java
+++ digitalneHodiny/digitalneHodiny/Cislica.java
@@ -18,6 +18,97 @@ public class Cislica {
     }
     
     public void zobraz(int cislica) {
-        
+        switch (cislica) {
+            case 0:
+                this.segmentA.setSvieti(true);
+                this.segmentB.setSvieti(true);
+                this.segmentC.setSvieti(true);
+                this.segmentD.setSvieti(true);
+                this.segmentE.setSvieti(true);
+                this.segmentF.setSvieti(true);
+                this.segmentG.setSvieti(false);
+                break;
+            case 1:
+                this.segmentA.setSvieti(false);
+                this.segmentB.setSvieti(true);
+                this.segmentC.setSvieti(true);
+                this.segmentD.setSvieti(false);
+                this.segmentE.setSvieti(false);
+                this.segmentF.setSvieti(false);
+                this.segmentG.setSvieti(false);
+                break;
+            case 2:
+                this.segmentA.setSvieti(true);
+                this.segmentB.setSvieti(true);
+                this.segmentC.setSvieti(false);
+                this.segmentD.setSvieti(true);
+                this.segmentE.setSvieti(true);
+                this.segmentF.setSvieti(false);
+                this.segmentG.setSvieti(true);
+                break;
+            case 3:
+                this.segmentA.setSvieti(true);
+                this.segmentB.setSvieti(true);
+                this.segmentC.setSvieti(true);
+                this.segmentD.setSvieti(true);
+                this.segmentE.setSvieti(false);
+                this.segmentF.setSvieti(false);
+                this.segmentG.setSvieti(true);
+                break;
+            case 4:
+                this.segmentA.setSvieti(false);
+                this.segmentB.setSvieti(true);
+                this.segmentC.setSvieti(true);
+                this.segmentD.setSvieti(false);
+                this.segmentE.setSvieti(false);
+                this.segmentF.setSvieti(true);
+                this.segmentG.setSvieti(true);
+                break;
+            case 5:
+                this.segmentA.setSvieti(true);
+                this.segmentB.setSvieti(false);
+                this.segmentC.setSvieti(true);
+                this.segmentD.setSvieti(true);
+                this.segmentE.setSvieti(false);
+                this.segmentF.setSvieti(true);
+                this.segmentG.setSvieti(true);
+                break;
+            case 6:
+                this.segmentA.setSvieti(true);
+                this.segmentB.setSvieti(false);
+                this.segmentC.setSvieti(true);
+                this.segmentD.setSvieti(true);
+                this.segmentE.setSvieti(true);
+                this.segmentF.setSvieti(true);
+                this.segmentG.setSvieti(true);
+                break;
+            case 7:
+                this.segmentA.setSvieti(true);
+                this.segmentB.setSvieti(true);
+                this.segmentC.setSvieti(true);
+                this.segmentD.setSvieti(false);
+                this.segmentE.setSvieti(false);
+                this.segmentF.setSvieti(false);
+                this.segmentG.setSvieti(false);
+                break;
+            case 8:
+                this.segmentA.setSvieti(true);
+                this.segmentB.setSvieti(true);
+                this.segmentC.setSvieti(true);
+                this.segmentD.setSvieti(true);
+                this.segmentE.setSvieti(true);
+                this.segmentF.setSvieti(true);
+                this.segmentG.setSvieti(true);
+                break;
+            case 9:
+                this.segmentA.setSvieti(true);
+                this.segmentB.setSvieti(true);
+                this.segmentC.setSvieti(true);
+                this.segmentD.setSvieti(true);
+                this.segmentE.setSvieti(false);
+                this.segmentF.setSvieti(true);
+                this.segmentG.setSvieti(true);
+                break;
+        }
     }
 }
