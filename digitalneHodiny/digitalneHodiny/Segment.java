03a949d514c3d37f7c17f8de1455703196493404
Jan Janech <jan.janech@fri.uniza.sk>
2019-10-17 15:10:06 +0200

    04b - dva druhy segmentov - nalezato/nastojato

--- digitalneHodiny/digitalneHodiny/Segment.java
+++ digitalneHodiny/digitalneHodiny/Segment.java
@@ -1,11 +1,15 @@
 public class Segment {
     private Obdlznik zobrazenie;
     
-    public Segment(int x, int y, int sirka, int vyska) {
+    public Segment(int x, int y, int dlzka, boolean jeNalezato) {
         this.zobrazenie = new Obdlznik();
         this.zobrazenie.posunZvisle(y - 50);
         this.zobrazenie.posunVodorovne(x - 60);
-        this.zobrazenie.zmenStrany(sirka, vyska);
+        if (jeNalezato) {
+            this.zobrazenie.zmenStrany(dlzka, 5);
+        } else {
+            this.zobrazenie.zmenStrany(5, dlzka);
+        }
         this.zobrazenie.zmenFarbu("black");
         this.zobrazenie.zobraz();
     }
