69c5bfa8d67875800a6b73bd0455000af9f8bd6d
Jan Janech <jan.janech@fri.uniza.sk>
2019-10-17 15:05:16 +0200

    04b - jednoducha verzia segmentu

--- digitalneHodiny/digitalneHodiny/Segment.java
+++ digitalneHodiny/digitalneHodiny/Segment.java
@@ -1,8 +1,20 @@
 public class Segment {
+    private Obdlznik zobrazenie;
+    
     public Segment(int x, int y, int sirka, int vyska) {
+        this.zobrazenie = new Obdlznik();
+        this.zobrazenie.posunZvisle(y - 50);
+        this.zobrazenie.posunVodorovne(x - 60);
+        this.zobrazenie.zmenStrany(sirka, vyska);
+        this.zobrazenie.zmenFarbu("black");
+        this.zobrazenie.zobraz();
     }
     
     public void setSvieti(boolean svieti) {
-        
+        if (svieti) {
+            this.zobrazenie.zmenFarbu("yellow");
+        } else {
+            this.zobrazenie.zmenFarbu("black");
+        }
     }
 }
