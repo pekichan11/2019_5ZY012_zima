528b044f01f95c3670539468510e1d4362ebf667
Jan Janech <jan.janech@fri.uniza.sk>
2019-10-24 15:44:25 +0200

    05b - spravny vypocet IBAN

--- banka/Banka.java
+++ banka/Banka.java
@@ -1,3 +1,6 @@
+import java.util.Random;
+import java.math.BigInteger;
+
 public class Banka {
     private String nazov;
     private String kodBanky;
@@ -12,12 +15,26 @@ public class Banka {
     }
     
     public Ucet zalozUcet(Klient vlastnik) {
+        Random nahodneCisla = new Random();
+        
+        long cisloUctu = Math.abs(nahodneCisla.nextLong()) % 10000000000L;
+        
+        String ibanPreVypocetKC = String.format(
+            "%s000000%010d%02d%02d00",
+            this.kodBanky.substring(2),
+            cisloUctu,
+            this.kodBanky.charAt(0) - 55,
+            this.kodBanky.charAt(1) - 55
+        );
+        
+        int kontrolneCislo = 98 - new BigInteger(ibanPreVypocetKC).mod(new BigInteger("97")).intValue();
+        
         String iban = String.format(
             "%s%02d%s000000%010d",
             this.kodBanky.substring(0, 2),
-            0,
+            kontrolneCislo,
             this.kodBanky.substring(2),
-            123
+            cisloUctu
         );
         return new Ucet(this, vlastnik, iban);
     }
