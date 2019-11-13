f2030cdaccf3f1d91adf439346a1710174db8349
Jan Janech <jan.janech@fri.uniza.sk>
2019-11-13 10:43:59 +0100

    08a - operacie s dverami

--- montyHill/Dvere.java
+++ montyHill/Dvere.java
@@ -1,15 +1,51 @@
-
-/**
- * Write a description of class Dvere here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
 public class Dvere {
-    /**
-     * Constructor for objects of class Dvere
-     */
-    public Dvere() {
-        // initialise instance variables
+    private boolean suVyherne;
+    private boolean suOtvorene;
+    private boolean suVybrane;
+    
+    public Dvere(boolean suVyherne) {
+        this.suVyherne = suVyherne;
+        this.suOtvorene = false;
+        this.suVybrane = false;
+    }
+    
+    public void otvor() {
+        if (this.suOtvorene) {
+            System.out.println("Dvere uz su otvorene");
+            return;
+        }
+        if (this.suVyherne) {
+            System.out.println("Tieto nemozes isto otvorit");
+            return;
+        }
+        if (this.suVybrane) {
+            System.out.println("Tie si uz vybral hrac");
+            return;
+        }
+        this.suOtvorene = true;
+    }
+    
+    public void vyber() {
+        if (this.suOtvorene) {
+            System.out.println("Dvere uz su otvorene");
+            return;
+        }
+        if (this.suVybrane) {
+            System.out.println("Tie si si uz vybral");
+            return;
+        }
+        this.suVybrane = true;
+    }
+    
+    public void zrusVyber() {
+        if (!this.suVybrane) {
+            System.out.println("Dvere nie su vybrane");
+            return;
+        }
+        this.suVybrane = false;
+    }
+    
+    public boolean jeVyhra() {
+        return this.suVybrane && this.suVyherne;
     }
 }
