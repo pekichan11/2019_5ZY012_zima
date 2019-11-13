7f159f9680fc6b4e34cfbe490b1fc0bd4f7c4489
Jan Janech <jan.janech@fri.uniza.sk>
2019-11-13 10:25:54 +0100

    08a - projekt pre monty hill problem

---
+++ montyHill/Dvere.java
@@ -0,0 +1,15 @@
+
+/**
+ * Write a description of class Dvere here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Dvere {
+    /**
+     * Constructor for objects of class Dvere
+     */
+    public Dvere() {
+        // initialise instance variables
+    }
+}
---
+++ montyHill/Sutaz.java
@@ -0,0 +1,15 @@
+
+/**
+ * Write a description of class Sutaz here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Sutaz {
+    /**
+     * Constructor for objects of class Sutaz
+     */
+    public Sutaz() {
+        // initialise instance variables
+    }
+}
